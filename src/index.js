6ad80ad8737c63cc68c14874d15888201aa08a81
espinoza-josafat <38735885+espinoza-josafat@users.noreply.github.com>
2018-08-09 15:30:08 -0500

    m
    m

--- src/index.js
+++ src/index.js
@@ -1,16 +1,86 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Button } from "reactstrap";
+
+/*import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope, faFacebook } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";*/
+
+//import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { faAdjust } from '@fortawesome/free-solid-svg-icons';
+
+//import { Button } from "reactstrap";
 
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+//library.add(faAdjust);
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Button color="danger">Danger!</Button>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+          <div className="card card-signin my-5">
+            <div className="card-body">
+              <h5 className="card-title text-center">Sign In</h5>
+              <form className="form-signin">
+                <div className="form-label-group">
+                  <input
+                    type="email"
+                    id="inputEmail"
+                    className="form-control"
+                    placeholder="Email address"
+                    required
+                    autofocus
+                  />
+                  <label for="inputEmail">Email address</label>
+                </div>
+                <div className="form-label-group">
+                  <input
+                    type="password"
+                    id="inputPassword"
+                    className="form-control"
+                    placeholder="Password"
+                    required
+                  />
+                  <label for="inputPassword">Password</label>
+                </div>
+                <div className="custom-control custom-checkbox mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="customCheck1"
+                  />
+                  <label className="custom-control-label" for="customCheck1">
+                    Remember password
+                  </label>
+                </div>
+                <button
+                  className="btn btn-lg btn-primary btn-block text-uppercase"
+                  type="submit"
+                >
+                  Sign in
+                </button>
+                <hr className="my-4" />
+                <button
+                  className="btn btn-lg btn-google btn-block text-uppercase"
+                  type="submit"
+                >
+                  <FontAwesomeIcon icon="google" /> Sign in with Google
+                </button>
+                <button
+                  className="btn btn-lg btn-facebook btn-block text-uppercase"
+                  type="submit"
+                >
+                  <FontAwesomeIcon icon="facebook" />
+                  Sign in with Facebook
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
